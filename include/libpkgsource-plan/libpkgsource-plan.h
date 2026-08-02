a5cbbfede6329bc726f148370c395534abefe364
Alexandr Savca <alexandr.savca89@gmail.com>
2026-08-02 13:17:10 +0000

    api: add umbrella public header

---
+++ include/libpkgsource-plan/libpkgsource-plan.h
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Alexandr Savca
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+/*! \file libpkgsource-plan.h \brief Complete public projection API. */
+#pragma once
+
+#include <libpkgsource-plan/adapter.h>
