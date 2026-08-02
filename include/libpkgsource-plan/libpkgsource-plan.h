d8a68e82aec1f17e5086e47a8dfc4226af1db377
Alexandr Savca <alexandr.savca89@gmail.com>
2026-08-02 16:20:03 +0300

    include: fix Doxygen markup in libpkgsource-plan.h header

--- include/libpkgsource-plan/libpkgsource-plan.h
+++ include/libpkgsource-plan/libpkgsource-plan.h
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: 2026 Alexandr Savca
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-/*! \file libpkgsource-plan.h \brief Complete public projection API. */
+/*! \file libpkgsource-plan.h
+ *  \brief Complete public projection API.
+ */
 #pragma once
 
 #include <libpkgsource-plan/adapter.h>
